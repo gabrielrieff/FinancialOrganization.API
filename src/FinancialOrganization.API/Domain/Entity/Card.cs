b6566b531e9a5db20905125b9f5b13e854eaac59
Gabriel rieff <gabrielrieff1@gmail.com>
2025-05-19 11:40:52 -0300

    Add project files.

---
+++ src/FinancialOrganization.API/Domain/Entity/Card.cs
@@ -0,0 +1,32 @@
+﻿namespace FinancialOrganization.API.Domain.Entity;
+
+public class Card : EntityBase
+{
+    public string Name { get; private set; }
+
+    public Card(string name)
+    {
+        Name = name;
+
+        Validate();
+    }
+
+    #region Setters
+     public void setName(string name)
+    {
+        Name = name;
+        Validate();
+    }
+    #endregion
+
+
+    #region Valitadors
+    public void Validate()
+    {
+        if (string.IsNullOrWhiteSpace(Name))
+        {
+            throw new Exception("Name is required");
+        }
+    }
+    #endregion
+}
---
+++ src/FinancialOrganization.API/Domain/Entity/EntityBase.cs
@@ -0,0 +1,8 @@
+﻿namespace FinancialOrganization.API.Domain.Entity;
+
+public class EntityBase
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
